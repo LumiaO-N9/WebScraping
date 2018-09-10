44c59b33ef4d6480df45075f0086ae4c9188de64
LumiaO <zzk1044740758@live.com>
2018-09-10 17:25:51 +0800

    a simple function getTitle with "try except"

--- scrapetest.py
+++ scrapetest.py
@@ -1,6 +1,23 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except (HTTPError, URLError) as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title is None:
+    print("Title could not be found")
+else:
+    print(title)
