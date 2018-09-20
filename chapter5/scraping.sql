2c0a567628f929434f114782bede3899dca171b7
LumiaO <zzk1044740758@live.com>
2018-09-20 20:31:46 +0800

    201809202031

--- chapter5/scraping.sql
+++ chapter5/scraping.sql
@@ -6,3 +6,6 @@ content VARCHAR(10000),
 created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(id)
 );
+
+INSERT INTO pages (title, content) VALUES ("Test page title", "This is some test page content. It can be up to 10,000 characters long.");
+
