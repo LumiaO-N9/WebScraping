254f95a6c313af4a86dd3c9ffb73b02343f5dc9a
LumiaO <zzk1044740758@live.com>
2018-09-20 17:19:05 +0800

    Scraping table from wikipedia

---
+++ chapter5/ComparisonOfTextEditors.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import csv
+
+html = urlopen('http://en.wikipedia.org/wiki/Comparison_of_text_editors')
+bsObj = BeautifulSoup(html)
+table = bsObj.findAll('table', {'class': 'wikitable'})[0]
+rows = table.findAll('tr')
+with open('./editors.csv', 'w', newline='', encoding='utf-8') as csvFile:
+    for row in rows:
+        csvRow = []
+        for cell in row.findAll(['td', 'th']):
+            csvRow.append(cell.get_text())
+        print(csvRow)
+        csv.writer(csvFile).writerow(csvRow)
