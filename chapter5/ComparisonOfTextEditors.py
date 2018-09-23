832c17795332cf896b3c8a38cda33ac48e84d385
LumiaO <zzk1044740758@live.com>
2018-09-23 13:58:53 +0800

    http->https

--- chapter5/ComparisonOfTextEditors.py
+++ chapter5/ComparisonOfTextEditors.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
 
-html = urlopen('http://en.wikipedia.org/wiki/Comparison_of_text_editors')
+html = urlopen('https://en.wikipedia.org/wiki/Comparison_of_text_editors')
 bsObj = BeautifulSoup(html)
 table = bsObj.findAll('table', {'class': 'wikitable'})[0]
 rows = table.findAll('tr')
